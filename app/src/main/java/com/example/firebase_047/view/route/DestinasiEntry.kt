de98e2e898ae1ca1f8da80832d9d330635e1612b
ibnatysalsabila <salsajpr01@gmail.com>
2026-01-02 17:32:08 +0700

    membuat destinasiedit

---
+++ app/src/main/java/com/example/firebase_047/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.firebase_047.view.route
+
+import com.example.firebase_047.view.route.DestinasiNavigasi
+import com.example.firebase_047.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
