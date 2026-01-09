d793eb76c998c2c2cba0ca22ee39d1d17dc1008a
ibnatysalsabila <salsajpr01@gmail.com>
2026-01-09 15:27:23 +0700

    menampilkan BodyDetailDataSiswa dan menangani aksi hapus data

--- app/src/main/java/com/example/firebase_047/view/HalamanDetail.kt
+++ app/src/main/java/com/example/firebase_047/view/HalamanDetail.kt
@@ -80,3 +80,18 @@ fun DetailSiswaScreen(
         modifier = modifier
     ) { innerPadding ->
         val coroutineScope = rememberCoroutineScope()
+
+        BodyDetailDataSiswa(
+            statusUIDetail = uiState,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.hapusSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
